7c11b1cb4c4c1b22769a3b2ead538a4c90082443
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-15 19:25:51 +0300

    Adding mongodb

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -61,6 +61,7 @@ func (uc *UserController) UpdateUser(ctx *gin.Context) {
 
 func (uc *UserController) DeleteUser(ctx *gin.Context) {
 	username := ctx.Param("name")
+	uc.UserService.DeleteUser(&username)
 	ctx.JSON(200, "")
 }
 
