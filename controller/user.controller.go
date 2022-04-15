612dbca75f13cd26fca707c136924cd22f4a1a76
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-15 18:51:07 +0300

    Adding mongodb

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -25,7 +25,7 @@ func (uc *UserController) CreateUser(ctx *gin.Context) {
 	}
 	err := uc.UserService.CreateUser(&user)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
 		return
 	}
 	ctx.JSON(200, "")
