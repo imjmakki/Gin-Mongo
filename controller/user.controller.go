44068937435011d6213c0f6362a82823f22d5aa3
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-15 18:27:08 +0300

    Adding mongodb

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -1,6 +1,9 @@
 package controller
 
-import "Gin-Mongo/service"
+import (
+	"Gin-Mongo/model"
+	"Gin-Mongo/service"
+)
 
 type UserController struct {
 	UserService service.UserService
@@ -10,4 +13,24 @@ func New(userService service.UserService) UserController {
 	return UserController{
 		UserService: userService,
 	}
+}
+
+func (u *UserServiceImpl) CreateUser(user *model.User) {
+	return nil
+}
+
+func (u *UserServiceImpl) GetUser(name *string) {
+	return nil, nil
+}
+
+func (u *UserServiceImpl) GetAll() {
+	return nil, nil
+}
+
+func (u *UserServiceImpl) UpdateUser(user *model.User) {
+	return nil
+}
+
+func (u *UserServiceImpl) DeleteUser(name *string) {
+	return nil
 }
