f1f643b26f1bde5282ab0b257c696a021d580974
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-15 16:46:02 +0300

    Adding mongodb

--- model/user.go
+++ model/user.go
@@ -6,7 +6,7 @@ type Address struct {
 }
 
 type User struct {
-	Name    string `json:"name" bson:"user_name"`
-	Age     int    `json:"age" bson:"user_age"`
-	Address Address
+	Name    string  `json:"name" bson:"user_name"`
+	Age     int     `json:"age" bson:"user_age"`
+	Address Address `json:"address" bson:"user_address"`
 }
