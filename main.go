c05c76da2af1c3d8f6e94726d1bd388296499d13
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-15 20:09:27 +0300

    Adding mongodb

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 )
 
@@ -25,6 +26,10 @@ func init() {
 
 	mongocon := options.Client().ApplyURI("mongodb://localhost:27017")
 	mongoclient, err = mongo.Connect(ctx, mongocon)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = mongoclient.Ping(ctx, readpref.Primary())
 	if err != nil {
 		log.Fatal(err)
 	}
