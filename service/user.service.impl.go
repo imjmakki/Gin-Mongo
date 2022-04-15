08041698dc803a00ea1770023c51349a37e8a853
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-15 19:17:05 +0300

    Adding mongodb

--- service/user.service.impl.go
+++ service/user.service.impl.go
@@ -37,6 +37,7 @@ func (u *UserServiceImpl) GetAll() ([]*model.User, error) {
 
 func (u *UserServiceImpl) UpdateUser(user *model.User) error {
 	filter := bson.D{bson.E{Key: "user_name", Value: user.Name}}
+	update := bson.D{bson.E{Key: "$set", Value: bson.D{}}}
 	return nil
 }
 
