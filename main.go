732f4450669768cbc5d91f594fb13cb93554ec09
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-15 20:00:42 +0300

    Adding mongodb

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"Gin-Mongo/controller"
+	"Gin-Mongo/service"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	server         *gin.Engine
+	userservice    service.UserService
+	usercontroller controller.UserController
+)
+
 func main() {
 
 }
